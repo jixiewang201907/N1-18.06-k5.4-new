ab231b13eba0e1d6b9e829eadfcdae2b9a18e193
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-18 18:10:41 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -15,7 +15,7 @@ git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/l
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # Add luci-app-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/passwalldp
-#git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/passwalldp/luci-app-passwall
+git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/passwalldp/luci-app-passwall
 git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/passwalldp/luci-app-passwall2
 # Add luci-app-bypass
 git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
