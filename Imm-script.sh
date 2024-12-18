a2d05462d8d3b07a880da2cdb7e8688fc3d55f9c
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-18 18:09:26 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd openwrt
 # wireless
-#rm -rf files/etc/config/wireless
-#rm -rf files/etc/modules.d/wireless_enable
+rm -rf files/etc/config/wireless
+rm -rf files/etc/modules.d/wireless_enable
 # geodata
 wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
