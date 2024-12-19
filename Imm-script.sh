af86cf8dbeab0d630bab677e03584046dcff69c7
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-19 13:38:36 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -4,8 +4,8 @@ cd openwrt
 rm -rf files/etc/config/wireless
 rm -rf files/etc/modules.d/wireless_enable
 # geodata
-wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 # Add luci-theme-opentomcat
 git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/luci-theme-opentomcat
 # Add luci-theme-argon
@@ -14,9 +14,8 @@ git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/l
 # Add amlogic
 #git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # Add luci-app-passwall
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
+#git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+#git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 # Add luci-app-bypass
 git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
 # Add luci-app-smartdns
@@ -24,3 +23,6 @@ git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smar
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # Add luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+
+./scripts/feeds update -a
+./scripts/feeds install -a
