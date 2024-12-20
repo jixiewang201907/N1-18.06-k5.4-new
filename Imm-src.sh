bc0265c88667a64fc7e58df5a4ab57b3ef495d8b
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-20 12:57:52 +0800

    Update Imm-src.sh

--- Imm-src.sh
+++ Imm-src.sh
@@ -2,7 +2,8 @@
 cd openwrt
 # Add a feed source
 #echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
-echo "src-git kwrt_packages https://github.com/kiddin9/kwrt-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall_dp https://github.com/kenzok8/small.git;master" >> "feeds.conf.default"
+echo "src-git openwrt_packages https://github.com/kenzok8/openwrt-packages.git;master" >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
