025b3bc0e74c7aac851eafe4c7de02b8ff7804c9
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-22 11:54:59 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -3,6 +3,9 @@ cd openwrt
 # wireless
 rm -rf files/etc/config/wireless
 rm -rf files/etc/modules.d/wireless_enable
+# golang newversion
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 # geodata
 wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
@@ -21,3 +24,5 @@ git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smar
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # Add luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+./scripts/feeds install -a -f -p passwall_packages
+./scripts/feeds install luci-app-passwall
