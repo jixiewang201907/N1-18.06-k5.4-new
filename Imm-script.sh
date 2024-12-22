17cb726a321e82e56466c22cb157d2512dfbff5a
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-22 16:23:14 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -10,17 +10,20 @@ git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/l
 wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 # Add luci-theme-opentomcat,Add luci-theme-argon
-git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/luci-theme-opentomcat
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # Add amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+#git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # Add luci-app-passwall
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 # Add luci-app-bypass
-git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
+#git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
 # Add luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # Add luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+# add kenzok8
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
+git clone https://github.com/kenzok8/small.git package/small
