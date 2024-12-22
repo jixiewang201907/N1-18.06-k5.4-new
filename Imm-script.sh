657365b0af2aaf2dd6825ca82d69e7bb31c6b168
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-22 09:41:12 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -10,7 +10,7 @@ wget -q -cp files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-da
 git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/luci-theme-opentomcat
 # Add luci-theme-argon
 #rm -rf package/lean/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # Add amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # Add luci-app-passwall
