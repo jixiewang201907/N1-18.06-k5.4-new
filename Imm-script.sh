7e2ba590fb1e8080f45329b06b1207db32722328
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-12-23 13:24:42 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -19,25 +19,25 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 # Add luci-theme-opentomcat,Add luci-theme-argon
 #git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/luci-theme-opentomcat
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
 # add passwall2
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-# add kenzok8
-#git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
-#git clone https://github.com/kenzok8/small.git package/small
-#rm -rf package/kenzok8/adguardhome
-#rm -rf package/kenzok8/luci-app-adguardhome
-#rm -rf package/kenzok8/luci-app-amlogic
+
 # Add amlogic
 #git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
 # Add luci-app-passwall
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 #git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+
 # Add luci-app-bypass
 #git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
+
 # Add luci-app-smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 #git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+
 # Add luci-app-adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
